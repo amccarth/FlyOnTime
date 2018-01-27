ea3d3b50541b15c4cb2990a317410a0a6f9bf061
AJ <ajm0050@auburn.edu>
2018-01-27 15:35:42 -0600

    actual first commit

---
+++ app/src/main/java/adammccarthy/flyontime/flightInfoDisplayActivity.java
@@ -0,0 +1,45 @@
+package adammccarthy.flyontime;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class flightInfoDisplayActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.action_directions:
+                    mTextMessage.setText("Directions");
+                    return true;
+                case R.id.action_departure:
+                    mTextMessage.setText("Departure");
+                    return true;
+                case R.id.action_arrival:
+                    mTextMessage.setText("Arrival");
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flight_info_display);
+
+        mTextMessage = (TextView) findViewById(R.id.airportLabel);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
